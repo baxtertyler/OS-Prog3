9e8ef65802402c258ccb6fa6575f36ba8ff8930c
Tyler Baxter <tybaxter@calpoly.edu>
2025-02-23 11:52:34 -0800

    process addresses

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# reads from file and puts addresses in an array
 def processInput(filename):
     file = open(filename, "r")
     input = file.read()
@@ -5,10 +6,16 @@ def processInput(filename):
     lines = input.split("\n")
     return lines
 
+def getAddress(address):
+    offset = address & 255 # 255 is 11111111 in binary
+    page = (address >> 8) & 255
+    return (page, offset)
 
+
+# main program
 def main():
-    lines = processInput("input.txt")
-    print(lines)
+    for line in processInput("input.txt"):
+        print(getAddress(int(line, 10)))
 
 if __name__ == "__main__":
     main()
