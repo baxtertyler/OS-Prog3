59eeb63b08a9eef32bbdf41535e59b3612f79b3b
Tyler Baxter <tybaxter@calpoly.edu>
2025-02-23 11:44:42 -0800

    get input

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+def processInput(filename):
+    file = open(filename, "r")
+    input = file.read()
+    file.close()
+    lines = input.split("\n")
+    return lines
+
+
 def main():
-    print("Hello, World!")
+    lines = processInput("input.txt")
+    print(lines)
 
 if __name__ == "__main__":
     main()
